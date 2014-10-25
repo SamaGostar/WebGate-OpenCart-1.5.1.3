62b9ffd8f979a332846cc8a8c7303f3d70999d32
Masoud A <amini141@gmail.com>
2014-10-25 10:00:32 +0330

    Update zarinpalwg.php

--- catalog/controller/payment/zarinpalwg.php
+++ catalog/controller/payment/zarinpalwg.php
@@ -1,217 +1,154 @@
 <?php
-//require_once(DIR_SYSTEM.'library/nuSoap/nusoap.php');
-class ControllerPaymentZarinpalwg extends Controller {
+class ControllerPaymentZarinpal extends Controller {
 	protected function index() {
-		$this->language->load('payment/zarinpalwg');
+		$this->language->load('payment/zarinpal');
     	$this->data['button_confirm'] = $this->language->get('button_confirm');
 		
 		$this->data['text_wait'] = $this->language->get('text_wait');
 		$this->data['text_ersal'] = $this->language->get('text_ersal');
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/zarinpalwg.tpl')) {
-			$this->template = $this->config->get('config_template') . '/template/payment/zarinpalwg.tpl';
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/zarinpal.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/payment/zarinpal.tpl';
 		} else {
-			$this->template = 'default/template/payment/zarinpalwg.tpl';
+			$this->template = 'default/template/payment/zarinpal.tpl';
 		}
 		
 		$this->render();		
-}
-public function confirm() {
-		
+	}
+
+	public function confirm() {
 		$this->load->model('checkout/order');
-		
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 		
 		$this->load->library('encryption');
-		
 		$encryption = new Encryption($this->config->get('config_encryption'));
 		
-		
-		
-		
-		$this->data['Amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);		//echo $this->data['Amount'];
-			//$amount = intval($Amount) /$order_info['currency_value'];
-			if($this->currency->getCode()=='RLS') {
-		$this->data['Amount']=$this->data['Amount'] / 10;
-	}
-		
-		$this->data['PIN']=$this->config->get('zarinpalwg_PIN');
-		
-		
+		$this->data['Amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);
+		$this->data['PIN']=$this->config->get('zarinpal_PIN');
 		$this->data['ResNum'] = $this->session->data['order_id'];
-
 		$this->data['return'] = $this->url->link('checkout/success', '', 'SSL');
-		//$this->data['return'] = HTTPS_SERVER . 'index.php?route=checkout/success';
-		
 		$this->data['cancel_return'] = $this->url->link('checkout/payment', '', 'SSL');
-		//$this->data['cancel_return'] = HTTPS_SERVER . 'index.php?route=checkout/payment';
-
 		$this->data['back'] = $this->url->link('checkout/payment', '', 'SSL');
-
-		$amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);		//echo $this->data['Amount'];
-			//$amount = intval($Amount) /$order_info['currency_value'];
-			if($this->currency->getCode()=='RLS') {
-		$amount=$amount / 10;
-	}
-	
-		$this->load->model('checkout/order');
 		
-		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-	
-	
-	$Description = $order_info['comment'] . ' - '. $order_info['order_id'];
-	
-	
-	
-		$this->data['order_id'] = $encryption->encrypt($this->session->data['order_id']);
-		$callbackUrl  =  $this->url->link('payment/zarinpalwg/callback', 'order_id=' . $this->data['order_id'], 'SSL');
+		try {
+			$client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8'));
 		
-		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
+			$amount = intval($this->data['Amount'])/$order_info['currency_value'];
+			if ($this->currency->getCode() == 'RLS') {
+				$amount=$amount/10;
+			}
 
-	$result = $client->PaymentRequest(
-						array(
-								'MerchantID' 	=> $this->data['PIN'],
-								'Amount' 	=> $amount,
-								'Description' 	=> $Description,
-								'Email' 	=> '',
-								'Mobile' 	=> '',
-								'CallbackURL' 	=> $callbackUrl
-							)
-	);
-
-	//Redirect to URL You can do it also by creating a form
-
-
-
-	$PayPath = 'https://www.zarinpal.com/pg/StartPay/'.$result->Authority;
-	$Status = $result->Status;
-	
-	if($Status == 100 )
-	{
-
-		$this->data['action'] = $PayPath;
-		$json = array();
-		$json['success']= $this->data['action'];	
-	
-		$this->response->setOutput(json_encode($json));
-		
-		} else {
+			$this->data['order_id'] = $encryption->encrypt($this->session->data['order_id']);
+			//$callbackUrl = $this->url->link('payment/zarinpal/callback&order_id=' . $encryption->encrypt($this->session->data['order_id']));
+			$callbackUrl = $this->url->link('payment/zarinpal/callback', 'order_id=' . $this->data['order_id'], 'SSL');
 			
-			$this->CheckState($Status);
-			echo'ERR: '.$result->Status;
-			//die();
-		}
+			$result = $client->PaymentRequest(array(
+				'MerchantID' 	=> $this->data['PIN'],
+				'Amount' 	=> $amount,
+				'Description' 	=> 'خريد شماره: ' . $order_info['order_id'],
+				'Email' 	=> '',
+				'Mobile' 	=> '',
+				'CallbackURL' 	=> $callbackUrl
+			));
+			
+			// echo $client->getError();
 
-//
-		
-	
-		
-}
+			if ($result->Status == 100) {
+				$this->data['action'] = 'https://www.zarinpal.com/pg/StartPay/'. $result->Authority;
+				$json = array();
+				$json['success']= $this->data['action'];	
+			
+				$this->response->setOutput(json_encode($json));
+			} else {	
+				$this->CheckState($result->Status);
+				//die();
+			}
+		} catch (Exception $e) {
+			$json = array();
+			$json['error']= 'Can not connect to ZarinPal.<br>' . $e->getMessage();
+			$this->response->setOutput(json_encode($json));
+		}
+	}
 
 	public function CheckState($status) {
 		$json = array();
-		$json['error']= 'کد خطا :'.$status;
+
+		switch($status){
+			case "-1" :
+				$json['error']="اطلاعات ارسالی ناقص می باشند";
+				break;
+			case "-2" :
+				$json['error']="وب سرويس نا معتبر می باشد";
+				break;
+			case "0" :
+			 $json['error']="عمليات پرداخت طی نشده است";
+				break;
+			case "1" :
+				break;
+			case "-11" :
+				$json['error']="مقدار تراکنش تطابق نمی کند";
+				break;
+			case "-12" :
+				$json['error']="زمان پرداخت طی شده و کاربر اقدام به پرداخت صورتحساب ننموده است";
+				break;
+			default :
+				$json['error']= "خطای نامشخص. کد خطا: " . $status;
+				break;
+		}
 		$this->response->setOutput(json_encode($json));
+	}
 
-	
-	}	
-			
-		
-	
-
-
-function verify_payment($authority, $amount){
-
-	if($authority){
-		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
-		if ((!$client))
-			{echo  "Error: can not connect to zarinpalwg.<br>";return false;}
-		
-		else {
-			$this->data['PIN'] = $this->config->get('zarinpalwg_PIN');
-
-		
-		$res = $client->PaymentVerification(
-						  	array(
-									'MerchantID'	 => $data['PIN'],
-									'Authority' 	 => $authority,
-									'Amount'	 => $amount
-								)
-		);
-
-
-	
-		
-			$this->CheckState($res->Status);
-			
-			if($res==100)
-				return true;
-
-			else {
+	public function verify_payment($authority, $amount){
+		$amount=$amount/10;
+		if ($authority) {
+			try {
+				$client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8'));
+				$this->data['PIN'] = $this->config->get('zarinpal_PIN');
+				$result = $client->PaymentVerification(array(
+					'MerchantID'	 => $this->data['PIN'],
+					'Authority' 	 => $authority,
+					'Amount'	 => $amount
+				));
+				
+				if ($result->Status==100) {
+					return true;
+				} else {
+					$this->CheckState($result->Status);
+					return false;
+				}
+			} catch (Exception $e) {
+				echo 'Error: can not connect to ZarinPal.<br>' . $e->getMessage();
 				return false;
 			}
-		
+		} else {
+			return false;
 		}
-	} 
-	
-	else {
+
 		return false;
 	}
-	
-	
-	return false;
-}
 
 	public function callback() {
 		$this->load->library('encryption');
 
 		$encryption = new Encryption($this->config->get('config_encryption'));
+		$au = $this->request->get['Authority'];
+		$order_id = $encryption->decrypt($this->request->get['order_id']);
+		$MerchantID=$this->config->get('zarinpal_PIN');
+		$debugmod=false;
 		
-		//$order_id = $encryption->decrypt($this->request->get['order_id']);
-		$MerchantID=$this->config->get('zarinpalwg_PIN');
-	    if($_GET['Authority']!== ''){
-		$authority = $_GET['Authority'];
-		
-//Your Order ID
-$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
-          $order_id=$this->session->data['order_id'];
-        $this->load->model('checkout/order');
+		$this->load->model('checkout/order');
 		$order_info = $this->model_checkout_order->getOrder($order_id);
-	$amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);		//echo $this->data['Amount'];
-			//$amount = intval($Amount) /$order_info['currency_value'];
-			if($this->currency->getCode()=='RLS') {
-		$amount=$amount / 10;
-	
-			}
-		
-
-				$res = $client->PaymentVerification(
-						  	array(
-									'MerchantID'	 => $MerchantID,
-									'Authority' 	 => $authority,
-									'Amount'	 => $amount
-								)
-		);
-		
-		//$res = $client->verification($MerchantID,$amount,$au );
-		
 		//$Amount = $this->currency->format($order_info['total'], 'RLS', $order_info['value'], FALSE);
-		
-		
-		
-		$Ref = $res->RefID;
-		$Status = $res->Status;
-		if($Status == 100)// Your Peyment Code Only This Event
-		{
-				$this->model_checkout_order->confirm($order_id, $this->config->get('zarinpalwg_order_status_id'),'شماره رسيد ديجيتالي; : '.$Ref);
+
+		$Amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);		//echo $this->data['Amount'];
+		$amount = $Amount/$order_info['currency_value'];
+		if ($order_info) {
+			if(($this->verify_payment($au, $amount)) or ($debugmod==true)) {
+				$this->model_checkout_order->confirm($order_id, $this->config->get('zarinpal_order_status_id'),'شماره رسيد ديجيتالي; Authority: '.$au);
 				
-				$this->response->setOutput('<html><head><meta http-equiv="refresh" CONTENT="2; url=' . $this->url->link('checkout/success') . '"></head><body><table border="0" width="100%"><tr><td>&nbsp;</td><td style="border: 1px solid gray; font-family: tahoma;background: #EDEBFE; font-size: 14px; direction: rtl; text-align: right;">با تشکر پرداخت تکمیل شد.لطفا چند لحظه صبر کنید و یا  <a href="' . $this->url->link('checkout/success') . '"><b>اینجا کلیک نمایید</b></a></td><td>&nbsp;</td></tr><tr><td colspan="2"> شماره رسيد ديجيتالي:'.$Ref.'</td></tr></table></body></html>');
+				$this->response->setOutput('<html><head><meta http-equiv="refresh" CONTENT="2; url=' . $this->url->link('checkout/success') . '"></head><body><table border="0" width="100%"><tr><td>&nbsp;</td><td style="border: 1px solid gray; font-family: tahoma; font-size: 14px; direction: rtl; text-align: right;">با تشکر پرداخت تکمیل شد.لطفا چند لحظه صبر کنید و یا  <a href="' . $this->url->link('checkout/success') . '"><b>اینجا کلیک نمایید</b></a></td><td>&nbsp;</td></tr></table></body></html>');
 			}
-		else {
-			$this->response->setOutput('<html><body><table border="0" width="100%"><tr><td>&nbsp;</td><td style="border: 1px solid gray;background: #EDEBFE; font-family: tahoma; font-size: 14px; direction: rtl; text-align: right;">خطا در عملیات پردازش پرداخت:<br />کد خطا:'.$Status.'<br /><br /><a href="' . $this->url->link('checkout/cart').  '"><b>بازگشت به فروشگاه</b></a></td><td>&nbsp;</td></tr></table></body></html>');
-		}
-	}
-		 else {
-			$this->response->setOutput('<html><body><table border="0" width="100%"><tr><td>&nbsp;</td><td style="border: 1px solid gray;background: #EDEBFE; font-family: tahoma; font-size: 14px; direction: rtl; text-align: right;">.		بازگشت از عمليات پرداخت، خطا در انجام عملیات پرداخت ( پرداخت ناموق ) !<br /><br /><a href="' . $this->url->link('checkout/cart').  '"><b>بازگشت به فروشگاه</b></a></td><td>&nbsp;</td></tr></table></body></html>');
+		} else {
+			$this->response->setOutput('<html><body><table border="0" width="100%"><tr><td>&nbsp;</td><td style="border: 1px solid gray; font-family: tahoma; font-size: 14px; direction: rtl; text-align: right;">.<br /><br /><a href="' . $this->url->link('checkout/cart').  '"><b>بازگشت به فروشگاه</b></a></td><td>&nbsp;</td></tr></table></body></html>');
 		}
 	}
 	
