af0eebc4f7e177ce34448e350462639640deb22e
Masoud Amini <amini141@gmail.com>
2016-06-19 12:18:36 +0430

    Update zarinpal.php

--- admin/controller/payment/zarinpal.php
+++ admin/controller/payment/zarinpal.php
@@ -96,7 +96,7 @@ class ControllerPaymentZarinpal extends Controller {
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('payment/zarinpal', $data));
+		$this->response->setOutput($this->load->view('payment/zarinpal.tpl', $data));
 	}
 
 	protected function validate() {
